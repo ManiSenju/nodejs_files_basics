dbb1f3ddedf7494f6943b8138df6df18a77791c0
Lokam Manikanta Sai <35286076+ManiSenju@users.noreply.github.com>
2022-10-27 12:36:22 +0530

    Add files via upload

--- Files/renameFiles/index.js
+++ Files/renameFiles/index.js
@@ -1,1 +1,22 @@
+const fs = require("fs")
+// rename for both files and directories
+fs.renameSync('./config.js', './renamedConfig.js')
 
+fs.rename('./config.js', './renamedConfig.js', err => {
+    if (err) {
+        throw err
+    } else {
+        console.log("renamed")
+    }
+})
+
+fs.renameSync('./test/reconfig.js', './reconfig.js')
+
+fs.unlinkSync('./renamedConfig.js')
+fs.unlink('./reconfig.js', err => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("file removed")
+    }
+})
